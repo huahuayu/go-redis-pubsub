b9ad33e3c07ecb7814c01b9b20edcc990929cd8d
shiming <codingmylife@gmail.com>
2021-01-15 12:00:59 +0800

    problem solved

--- README.md
+++ README.md
@@ -1,48 +1,6 @@
-## Problem
+# go redis pubsub example
 
-go-redis pub sub not working, no message write to the queue.
-
-**step1:** create three user instance 
-
-```go
-	alice := &User{"alice", "female", 18}
-	bob := &User{"bob", "male", 20}
-	frank := &User{"frank", "male", 20}
-```
-
-**step2:** put alice in channel: testChannel1:key1:key2:key3
-
-```go
-	n,err := Client.Publish(ctx,"testChannel1:key1:key2:key3", string(aliceBytes)).Result()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Info(n)
-```
-
-**step3:** put bob & frank in channel: testChannel2:key1:key2:key3
-
-**step4:** listen to both channels.
-
-```go
-	ch := Client.Subscribe(ctx,"testChannel1:key1:key2:key3","testChannel2:key1:key2:key3").Channel()
-	for msg := range ch {
-		fmt.Println(msg.Channel, msg.Payload)
-	}
-```
-
-the output:
-
-```text
-time="2021-01-14T16:24:48+08:00" level=info msg=0
-time="2021-01-14T16:24:48+08:00" level=info msg=0
-time="2021-01-14T16:24:48+08:00" level=info msg=0
-```
-
-no message write to the queue, thus no message can read from it.
-
-
-## Reproduce
+## get started
 
 clone this example
 
@@ -52,10 +10,12 @@ git clone https://github.com/huahuayu/go-redis-pubsub.git
 
 run test in `redis_test.go`
 
-```bash
-cd go-redis-pubsub/redis
-go test
-```
+outoput:
 
+```text
+    redis_test.go:46: testChannel1:key1:key2:key3 {"Name":"alice","Gender":"female","Age":18}
+    redis_test.go:46: testChannel2:key1:key2:key3 {"Name":"bob","Gender":"male","Age":20}
+    redis_test.go:46: testChannel2:key1:key2:key3 {"Name":"frank","Gender":"male","Age":20}
+```
 
 
--- redis/redis_test.go
+++ redis/redis_test.go
@@ -3,44 +3,75 @@ package redis
 import (
 	"context"
 	"encoding/json"
-	"fmt"
-	log "github.com/sirupsen/logrus"
 	"testing"
+	"time"
 )
 
-func TestSubPub(t *testing.T) {
+func publish(ctx context.Context, channel string, message interface{}) {
+	// Publish a message.
+	err := Client.Publish(ctx, channel, message).Err()
+	if err != nil {
+		panic(err)
+	}
+}
+func TestPubSub(t *testing.T) {
+	Init()
 	type User struct {
 		Name   string
 		Gender string
 		Age    int
 	}
-	Init()
 	alice := &User{"alice", "female", 18}
 	bob := &User{"bob", "male", 20}
 	frank := &User{"frank", "male", 20}
 	aliceBytes, _ := json.Marshal(alice)
 	bobBytes, _ := json.Marshal(bob)
 	frankBytes, _ := json.Marshal(frank)
+	ctx := context.TODO()
+	pubsub := Client.Subscribe(ctx, "testChannel1:key1:key2:key3", "testChannel2:key1:key2:key3")
+	// Wait for confirmation that subscription is created before publishing anything.
+	_, err := pubsub.Receive(ctx)
+	if err != nil {
+		panic(err)
+	}
+	publish(ctx,  "testChannel1:key1:key2:key3", string(aliceBytes))
+	publish(ctx,  "testChannel2:key1:key2:key3", string(bobBytes))
+	publish(ctx,  "testChannel2:key1:key2:key3", string(frankBytes))
+	time.AfterFunc(time.Second, func() {
+		// When pubsub is closed channel is closed too.
+		_ = pubsub.Close()
+	})
+	// Consume messages.
+	for msg := range pubsub.Channel() {
+		t.Log(msg.Channel, msg.Payload)
+	}
+}
 
-	ctx := context.Background()
-	n,err := Client.Publish(ctx,"testChannel1:key1:key2:key3", string(aliceBytes)).Result()
-	if err != nil {
-		log.Fatal(err)
+func TestSub(t *testing.T){
+	Init()
+	ctx := context.TODO()
+	pubsub := Client.Subscribe(ctx, "testChannel1:key1:key2:key3", "testChannel2:key1:key2:key3")
+	// Consume messages.
+	for msg := range pubsub.Channel() {
+		t.Log(msg.Channel, msg.Payload)
 	}
-	log.Info(n)
-	n,err = Client.Publish(ctx,"testChannel2:key1:key2:key3", string(bobBytes)).Result()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Info(n)
-	n,err = Client.Publish(ctx,"testChannel2:key1:key2:key3", string(frankBytes)).Result()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Info(n)
+}
 
-	ch := Client.Subscribe(ctx,"testChannel1:key1:key2:key3","testChannel2:key1:key2:key3").Channel()
-	for msg := range ch {
-		fmt.Println(msg.Channel, msg.Payload)
+func TestPub(t *testing.T){
+	Init()
+	type User struct {
+		Name   string
+		Gender string
+		Age    int
 	}
+	alice := &User{"alice", "female", 18}
+	bob := &User{"bob", "male", 20}
+	frank := &User{"frank", "male", 20}
+	aliceBytes, _ := json.Marshal(alice)
+	bobBytes, _ := json.Marshal(bob)
+	frankBytes, _ := json.Marshal(frank)
+	ctx := context.TODO()
+	publish(ctx,  "testChannel1:key1:key2:key3", string(aliceBytes))
+	publish(ctx,  "testChannel2:key1:key2:key3", string(bobBytes))
+	publish(ctx,  "testChannel2:key1:key2:key3", string(frankBytes))
 }
